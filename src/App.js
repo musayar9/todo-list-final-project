061065af90a6f25fdc2f5410194bc38e48705710
Musa SAYAR <musasayar67@gmail.com>
2023-08-28 11:55:39 +0300

    layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-
-import './App.css';
+import "./App.css";
 import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center mt-5 ">
-    <h1 className='text-5xl font-semibold underline text-white italic'>Todo List</h1>
-     <TodoList/>
+      <h1 className="text-5xl font-semibold underline  drop-shadow-2xl text-white italic">
+        Todo List
+      </h1>
+      <TodoList />
     </div>
   );
 }
